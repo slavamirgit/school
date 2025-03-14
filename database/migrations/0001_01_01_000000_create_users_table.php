4838c202523a80492f7f568ca43eef11cc881c65
slavamir <slava.acct@gmail.com>
2025-03-14 13:23:18 +0200

    Process

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Role::class)->default(2)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Role::class)->default(2);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
